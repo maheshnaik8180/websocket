7cea1ea8cd6c97b9b93515c0fc9c27b68713dcb0
GENIUS <naikmahesh.naik@gmail.com>
2021-05-08 16:48:40 +0530

    Update : server and client program add environment variables

--- clientside.py
+++ clientside.py
@@ -1,34 +1,55 @@
 import threading
 import socket
 import logging
-
+import os
+from dotenv import *
 from log import logger
+load_dotenv(find_dotenv())
 
-alias = input('Choose an alias >>> ')
+host = os.getenv('HOST')
+port = os.getenv('port')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 5798))
-
-def client_receive():
-    while True:
-        try:
-            message = client.recv(1024).decode('utf-8')
-            if message == "alias?":
-                client.send(alias.encode('utf-8'))
-            else:
-                logger.info(message)
-        except:
-            logger.info('Error!')
-            client.close()
-            break
+client.connect((host,port))
+alias = input('Choose an alias ')
 
 
-def client_send():
-    while True:
-        message = f'{alias}: {input("")}'
-        client.send(message.encode('utf-8'))
+logger.setLevel(logging.INFO)
+class clientside:
+    def client_receive(self):
+        """
+        recive the message and encode the message using utf-8
+        :return: returns
+        """
+        while True:
+            try:
+                message = client.recv(1024).decode('utf-8')
+                if message == "alias?":
+                    client.send(alias.encode('utf-8'))
+                else:
+                    logger.info(message)
+            except:
+                logger.info('Error!')
+                client.close()
+                break
 
-receive_thread = threading.Thread(target=client_receive)
-receive_thread.start()
 
-send_thread = threading.Thread(target=client_send)
-send_thread.start()
+    def client_send(self):
+        """
+                give the input and send the message
+                 :return: returns
+                """
+        while True:
+            message = f'{alias}: {input("")}'
+            client.send(message.encode('utf-8'))
+
+
+    receive_thread = threading.Thread(target=client_receive)
+    receive_thread.start()
+
+    send_thread = threading.Thread(target=client_send)
+    send_thread.start()
+
+clients = clientside()
+recive = clients.client_receive()
+
+send = clients.client_send()
--- serverside.py
+++ serverside.py
@@ -1,52 +1,125 @@
+"""
+date = '4/05/2021'
+modified_date = '5/05/2021'
+author = 'Mahesh Naik'
+description = '  using Websocket programming create client and server communication'
+"""
+
+import os
 import threading
 import socket
+import mysql.connector
 import logging
+from dotenv import *
 
 from log import logger
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 5798))
+load_dotenv(find_dotenv())
+HOST = os.getenv('HOST')
+PORT = os.getenv('PORT')
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_pass = os.getenv('DB_PASS')
+db_name = os.getenv('DB')
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(bytearray(HOST,PORT))
 server.listen()
 clients = []
 aliases = []
 
+class server:
+    # Constructor for Server class
+    def __init__(self, db_host, db_user, db_pass, db_name, logger):
+        self.conn = mysql.connector.connect(host=db_host, user=db_user, password=db_pass, database=db_name)
+        self.cursor = self.conn.cursor()
+        self.clients = []
+        self.aliases = []
+        self.logger = logger
 
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+        # Function to close database connection
 
-# Function to handle clients'connections
+    def close_conn(self):
+        """
+        parameter: conn =connection
+        close connection
+        """
+        self.conn.close()
 
-def handle_client(client):
-    while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            alias = aliases[index]
-            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
-            aliases.remove(alias)
-            break
-# Main function to receive the clients connection
+        # Function to display chat stored on database
+    def display(self):
+            """
+            display function
+            store chat in database
+            using sql query display the data
+            """
+            sql_query = "SELECT * FROM chat_data;"
+            try:
+                self.cursor.execute(sql_query)
+                result = self.cursor.fetchall()
+                for i in result:
+                    data = i[1]
+                    print(data)
+            except Exception:
+                self.logger.error('Error:Unable to fetch data.')
 
-def receive():
-    while True:
-        logger.info('Server is running and listening ...')
-        client, address = server.accept()
-        logger.info(f'connection is established with {str(address)}')
-        client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
-        aliases.append(alias)
-        clients.append(client)
-        logger.info(f'The alias of this client is {alias}'.encode('utf-8'))
-        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
-        client.send('you are now connected!'.encode('utf-8'))
-        thread = threading.Thread(target=handle_client, args=(client,))
-        thread.start()
+    def broadcast(self,message,connection):
+        """
+                  broadcast the messasge given by clientside message
+                  :return: returns
+                  """
+        for client in self.clients:
+            if (client != connection):
+                    client.send(message)
+
+    # Function to handle clients'connections
+
+
+    def handle_client(self,client):
+        """
+        handle client use buffer string
+        """
+        while(True):
+            try:
+                message = client.recv(1024)
+                self.broadcast(message)
+            except:
+                index = self.cursor.index(client)
+                clients.remove(client)
+                client.close()
+                alias = aliases[index]
+                self.cursor(f'{alias} has left the chat room!'.encode('utf-8'))
+                aliases.remove(alias)
+                break
+            else:
+                sql_queery = "INSERT INTO clientA_clientB(chat) VALUES(' ');"
+
+                try:
+                    self.cursor.execute(sql_queery)
+                    self.cursor.commit()
+                except:
+                    self.cursor.rollback()
+                self.display()
+    # Main function to receive the clients connection
+
+    # Function to receive messages from clients
+    def receive(self):
+        while True:
+            logger.info('Server is running and listening ...')
+            client, address = server.accept()
+            logger.info(f'connection is established with {str(address)}')
+            client.send('alias?'.encode('utf-8'))
+            alias = client.recv(1024)
+            aliases.append(alias)
+            clients.append(client)
+            logger.info(f'The alias of this client is {alias}'.encode('utf-8'))
+            self.display(f'{alias} has connected to the chat room'.encode('utf-8'))
+            client.send('you are now connected!'.encode('utf-8'))
+            thread = threading.Thread(target=self.display(), args=(client,))
+            thread.start()
+
 
 if __name__ == "__main__":
+    dbconn = server(db_host, db_user, db_pass, db_name, logger)
     logger.setLevel(logging.INFO)
-    receive()
+    dbconn.receive()
